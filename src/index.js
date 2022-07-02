3eb8b3225f3176a624cb387aaaf0e2f32c5fb9f1
Sai Meghana Vemula <meghana.bamr@gmail.com>
2022-07-02 21:50:02 +0530

    update

--- src/index.js
+++ src/index.js
@@ -2,12 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import SeasonDisplay from './SeasonDisplay';
 
-const App = () => {
+// const App = () => {
   
+//     window.navigator.geolocation.getCurrentPosition(
+//       (position) => console.log(position),
+//       err => console.log(err)
+//     )
+//       return <div>Latitide: </div>
+// }
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    
+    this.state = {lat : null };
     window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
+      (position) => 
+      {
+        //called setState
+        this.setState({lat:position.coords.latitude}
+          //we didnt update
+          //To update do not use this.state.lat = position.coords.latitude
+
+          )
+      },
       err => console.log(err)
-    )
-      return <div>Latitide: </div>
+);  
+  }
+  render(){
+    
+    return <div>Latitude: {this.state.lat}</div>
+  }
 }
-ReactDOM.createRoot(document.querySelector('#root')).render(App()) 
+const root =
+ReactDOM.createRoot(document.querySelector('#root'))
+root.render(<App />);
